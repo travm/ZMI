cca0cec1d352787882019853dc5af77b69309f42
travm <mayzard@gmail.com>
2013-07-10 11:14:27 -0400

    Completed calculations for early release.

--- js/main.js
+++ js/main.js
@@ -28,17 +28,26 @@ $(function(){
 
             // Store BMI in a variable for BMI Z-Score calculation
             var bmi = calculateBMI(weight, weightUnit, height, heightUnit);
+            var bmiRounded = Math.round(bmi*100)/100;
 
             // Store BMI Z-Score for MetS Calculation
             var zScore = calculateZscore(age, gender, bmi);
+            var zScoreRounded = Math.round(zScore*100)/100;
 
             // Store MetS Score for Output
             var metsScore = calculateMets(gender, race, zScore, tri, hdl, sbp, glu);
+            var metsScoreRounded = Math.round(metsScore*100)/100;
 
             // Calculate MetS Z-Score Percentage
-            var metsPercentile = ((1/(Math.sqrt(2*3.14)))*(Math.pow(2.718281828, -(Math.pow(0.5181125828466573, 2)/2)))*2);
+            var metsPercentile = ((1/(Math.sqrt(2*3.14)))*(Math.pow(2.718281828, -(Math.pow(zScore, 2)/2)))*2)*100;
+            var metsPercentileRounded = Math.round(metsPercentile*100)/100;
 
-            result.text("MetS Score: " + metsScore + "MetS Percent: " + metsPercentile);
+            result.show().html(
+                '<table>' + 
+                '<tr><td>Your BMI<br /><span class="resultNum">' + bmiRounded + '</span></td><td>Your Z-Score<br /><span class="resultNum">' + zScoreRounded + "</span></td></tr>" +
+                '<tr><td>MetS Score<br /><span class="resultNum"> ' + metsScoreRounded + '</span></td><td>MetS Percent<br /><span class="resultNum">' + metsPercentileRounded + "%</span></td></tr>" +
+                '</table>'
+            );
 
         }
     });
