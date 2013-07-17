a8cb78383d30766163c3d4feeeffd1e18cd3775b
travm <mayzard@gmail.com>
2013-07-17 16:54:19 -0400

    Added appointment and birthdate fields, added moment.js and fixed and completed percentile math.

--- js/main.js
+++ js/main.js
@@ -11,14 +11,16 @@ $(function(){
         e.preventDefault();
 
         var $form = $('form');
-        if($form.valid()) {
+        //if($form.valid()) {
     
             // Store Form Elements
             var weight = $('#weight').val();
             var height = $('#height').val();
             var weightUnit = $('input[name=weightUnit]:checked').val();
             var heightUnit = $('input[name=heightUnit]:checked').val();
-            var age = $('#age').val();
+            var birth = moment($("#birthdate").val());
+            var appointment = moment($('#appointment').val());
+            var age = appointment.diff(birth, 'months'); // Should this be age? Or should it be another variable?
             var gender = $('input[name=gender]:checked').val();
             var race = $('#race').val();
             var tri = $('#triglyceride').val();
@@ -31,25 +33,30 @@ $(function(){
             var bmiRounded = Math.round(bmi*100)/100;
 
             // Store BMI Z-Score for MetS Calculation
-            var zScore = calculateZscore(age, gender, bmi);
-            var zScoreRounded = Math.round(zScore*100)/100;
+            var bmiZScore = calculateZscore(age, gender, bmi);
+            var bmiZScoreRounded = Math.round(bmiZScore*100)/100;
+
+            // Store BMI Z-Score Percentile
+            var bmiPercentile = 100*(1/(1+Math.exp(-0.07056 * Math.pow(bmiZScore, 3) - (1.5976*bmiZScore))));
+            var bmiPercentileRounded = Math.round(bmiPercentile*100)/100;
 
             // Store MetS Score for Output
-            var metsScore = calculateMets(gender, race, zScore, tri, hdl, sbp, glu);
-            var metsScoreRounded = Math.round(metsScore*100)/100;
+            var metsZScore = calculateMets(gender, race, bmiZScore, tri, hdl, sbp, glu);
+            var metsZScoreRounded = Math.round(metsZScore*100)/100;
 
             // Calculate MetS Z-Score Percentage
-            var metsPercentile = ((1/(Math.sqrt(2*3.14)))*(Math.pow(2.718281828, -(Math.pow(zScore, 2)/2)))*2)*100;
+            var metsPercentile = 100*(1/(1+Math.exp(-0.07056 * Math.pow(metsZScore, 3) - (1.5976*metsZScore))));
             var metsPercentileRounded = Math.round(metsPercentile*100)/100;
+            console.log("MetS Percentile: " + metsPercentile);
+            console.log("MetS Percentile Rounded: " + metsPercentileRounded);
+
 
             result.show().html(
-                '<table>' + 
-                '<tr><td>Your BMI<br /><span class="resultNum">' + bmiRounded + '</span></td><td>Your Z-Score<br /><span class="resultNum">' + zScoreRounded + "</span></td></tr>" +
-                '<tr><td>MetS Score<br /><span class="resultNum"> ' + metsScoreRounded + '</span></td><td>MetS Percent<br /><span class="resultNum">' + metsPercentileRounded + "%</span></td></tr>" +
-                '</table>'
+                '<table><tr><td>Your BMI<br /><span class="resultNum">' + bmiRounded + '</span></td><td>Your Z-Score<br /><span class="resultNum">' + bmiZScoreRounded + '</span></td><td>MetS Z-Score<br /><span class="resultNum"> ' + metsZScoreRounded + '</span></td></tr></table>' +
+                '<table><tr><td>BMI Percentile<br /><span class="resultNum"> ' + bmiPercentileRounded + '%</span></td><td>MetS Percentile<br /><span class="resultNum">' + metsPercentileRounded + "%</span></td></tr></table>"
             );
 
-        }
+        //}
     });
 
 });
